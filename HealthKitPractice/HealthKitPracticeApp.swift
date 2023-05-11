25861e4e971e2a31025478cbde1d5bd940185587
Berkay-Disli <berkaydisli.work@gmail.com>
2023-05-11 17:17:55 +0300

    Initial Commit

---
+++ HealthKitPractice/HealthKitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitPracticeApp.swift
+//  HealthKitPractice
+//
+//  Created by Berkay Disli on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
